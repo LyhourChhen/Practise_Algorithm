ae35d1000d28f5eb55f64f6eb9fefcda201a18bc
LyhourChhen <ukamilove66@gmail.com>
2020-02-22 13:25:55 +0700

    simpleArraySum

--- ReverseString/reverseString.py
+++ ReverseString/reverseString.py
@@ -7,6 +7,8 @@ Create the function which can enter the string and reverse it back !
 
 print("Reverse String")
 
+# My Approuch which totally messy bullshit and it doesn't not work : ((((
+
 def reverseString(arg):
     print("Normal String : ", arg)
     #Find how to many in range first
@@ -20,4 +22,30 @@ def reverseString(arg):
         return
     return result
 print("result :", reverseString("LyhourChhen"))
+
+#method 1 (slicing)
+
+# Slicing in python can be easily to reverse the index ! :S
+
+str="Python" # initial string
+stringlength=len(str) # calculate length of the list => 6
+#stringname[stringlength::-1] (1st) 
+slicedString=str[stringlength::-1] # slicing (2nd)
+print (slicedString) # print the reversed string
+
+#method 2 (loop)
+
+str = "Python" # initial string
+reversedString=[]
+index = len(str) # calculate length of string and save in index
+while index > 0: 
+    reversedString += str[ index - 1 ] # save the value of str[index-1] in reverseString
+    index = index - 1 # decrement index
+print(reversedString) # reversed string
+
+
+#method 3 (join)
+str = 'Python' #initial string
+reversed=''.join(reversed(str)) # .join() method merges all of the characters resulting from the reversed iteration into a new string
+print(reversed) #print the reversed string
 
