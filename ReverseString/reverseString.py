24e1fb8499fb4177204028f5f5dda7d023fc5cef
LyhourChhen <ukamilove66@gmail.com>
2020-02-22 12:58:27 +0700

    simpleArraySum

---
+++ ReverseString/reverseString.py
@@ -0,0 +1,23 @@
+#!usr/bin/python3
+# Reverse String
+
+'''
+Create the function which can enter the string and reverse it back ! 
+'''
+
+print("Reverse String")
+
+def reverseString(arg):
+    print("Normal String : ", arg)
+    #Find how to many in range first
+    rangeAmount = 0
+    result = "lala"
+    for i in arg:
+        rangeAmount = i + i
+        return rangeAmount
+    #reverse loop
+    for i in range(rangeAmount,1,-1):
+        return
+    return result
+print("result :", reverseString("LyhourChhen"))
+
