38abf0d181ecad0f746a3ee5bd42e4cadd1c26f1
LyhourChhen <ukamilove66@gmail.com>
2020-02-22 21:56:19 +0700

    working with python basic

---
+++ print_python_version/what_is_python_version.py
@@ -0,0 +1,6 @@
+#!usr/bin/python3
+import sys  # sys library mostly working with the interpreter
+print("Print the python am i using !!")
+
+version = sys.version
+print("python i am using is ", version)
